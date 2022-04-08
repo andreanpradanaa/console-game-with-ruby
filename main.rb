3900e18afa04014d22501c3222304923aaebb686
andreanpradanaa <andreanpradana2001@gmail.com>
2022-04-09 00:21:07 +0700

    commit

--- main.rb
+++ main.rb
@@ -1,31 +1,101 @@
-require_relative "monster.rb"
-require_relative "player.rb"
+require_relative 'fire_monster'
+require_relative 'ice_monster'
+require_relative 'player'
+require_relative 'monster'
 
-phpkachu = Monster.new('Phpkachu', 80, 50)
-sqlrtle = Monster.new('Sqlrtle', 100, 20)
-cppmander = Monster.new('Cppmander', 40, 80)
-bashtoise = Monster.new('Bashtoise', 60, 60)
-torterraform = Monster.new('Torterraform', 120, 10)
+phpkachu = Monster.new('Phpkachu', 120, 50)
+sqlrtle = IceMonster.new('Sqlrtle', 180, 20)
+cppmander = FireMonster.new('Cppmander', 100, 80)
+bashtoise = IceMonster.new('Bashtoise', 120, 60)
+torterraform = Monster.new('Torterraform', 200, 10)
 
-puts phpkachu.attack_point
+player1 = Player.new('Player 1')
+player1.add_monster(sqlrtle)
+player1.add_monster(cppmander)
+player1.add_monster(torterraform)
 
-# player1 = Player.new("Player 1")
-# player1.add_monster(phpkachu)
-# player1.add_monster(sqlrtle)
-# player1.add_monster(torterraform)
+player2 = Player.new('Player 2')
+player2.add_monster(phpkachu)
+player2.add_monster(cppmander)
+player2.add_monster(bashtoise)
 
-# player2 = Player.new("Player 2")
-# player2.add_monster(sqlrtle)
-# player2.add_monster(cppmander)
-# player2.add_monster(bashtoise)
+puts '=======================Turn 1======================='
+puts ''
 
-# player1.status
-# player2.status
+puts player1
+puts ''
 
-# puts '=======================Turn 1======================='
-# puts ''
+puts player2
+puts ''
 
-# player1.attack(1, 2, player2)
-# player1.status
-# player2.status
+player1.play_turn(2, 1, player2)
+puts ''
 
+puts player1
+puts ''
+
+puts player2
+puts ''
+
+player2.play_turn(2, 3, player1)
+puts ''
+
+puts player1
+puts ''
+
+puts player2
+puts ''
+
+puts '=======================Turn 2======================='
+puts ''
+
+player1.apply_effects
+player2.apply_effects
+
+puts player1
+puts ''
+
+puts player2
+puts ''
+
+player1.play_turn(1, 3, player2)
+puts ''
+
+puts player1
+puts ''
+
+puts player2
+puts ""
+
+player2.play_turn(3, 2, player1)
+puts ''
+
+puts player1
+puts ''
+
+puts player2
+puts ''
+
+puts '=======================Turn 3======================='
+puts ''
+
+player1.apply_effects
+player2.apply_effects
+
+puts player1
+puts ''
+
+puts player2
+puts ''
+
+puts '=======================Turn 4======================='
+puts ''
+
+player1.apply_effects
+player2.apply_effects
+
+puts player1
+puts ''
+
+puts player2
+puts ''
--- player.rb
+++ player.rb
@@ -1,29 +1,41 @@
+require_relative 'monster'
+  
 class Player
-    attr_reader :monsters
+  attr_reader :monsters
 
-    def initialize(name)
-        @name = name
-        @monsters = []
+  def initialize(name)
+    @name = name
+    @monsters = []
+  end
+
+  def add_monster(monster)
+    monsters << monster.dup
+  end
+
+  def to_s
+    result = "#{@name}:\n"
+
+    @monsters.each_with_index do |monster, index|
+      result += monster.to_s
+      if index < @monsters.size - 1
+        result += ', '
+      else
+        result += "\n"
+      end
     end
-
     
-    def add_monster(monster)
-        @monsters << monster.dup
-    end
-    
-    def to_s
-        "#{@name}"
-    end
+    result
+  end
 
-    def status
-        puts to_s 
-        puts @monsters.join(", ")
-        puts " "
-    end
+  def play_turn(own_card, opponent_card, opponent)
+    print "#{@name}'s "
 
-    def attack(own_monster, opponent_monster, opponent)
-        print to_s + "'s "
-        @monsters[own_monster-1].attack(opponent.monsters[opponent_monster-1])
-        puts ""
+    @monsters[own_card-1].attack(opponent.monsters[opponent_card-1])
+  end
+
+  def apply_effects
+    @monsters.each do |monster|
+      monster.apply_effect
     end
+  end
 end
